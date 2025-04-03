2226821dc865c9756d6d50b03baa3baf60c84035
crazyraturi <subodhblogger966@gmail.com>
2025-04-03 08:50:22 +0530

    update 0.5.

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -47,6 +47,7 @@ const Overlay = ({ onClick }) => {
         src={backgroundVideo}
         muted
         playsInline
+        // poster="https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       />
 
      <div className=" z-20 fame-heading absolute flex items-center justify-center pt-7 gap-7 top-0">
@@ -73,14 +74,14 @@ const Overlay = ({ onClick }) => {
 
         {!isAnimating && (
           <button
-            className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#988579] text-white px-6 py-3 rounded-lg border-2 border-white hover:bg-[#7a6b61] z-20"
+            className="absolute left-1/2 top-1/2 transform uppercase  -translate-x-1/2 -translate-y-1/2 bg-[#988579] text-white px-6 py-3 rounded-lg border-2 border-white hover:bg-[#7a6b61] z-20"
             onClick={handleClick}
             style={{
               cursor: "pointer",
               boxShadow: "0 0 10px rgba(255,255,255,0.5)",
             }}
           >
-            Explore
+            click to Explore
           </button>
         )}
       </div>
